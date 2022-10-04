8a2c4ec0acd70e3f2b5eaac47334428691305f08
huanglin <2762565371@qq.com>
2022-10-04 22:17:00 +0800

    ✨ feat:完成修改商品分类，获取单个商品分类的接口

--- router/goods_cate.js
+++ router/goods_cate.js
@@ -4,18 +4,32 @@ const expressJoi = require("@escook/express-joi");
 const {
 	goods_cate_id,
 	goods_cate_add,
+	goods_cate_update,
 	goods_cate_batch_delete
-} = require("../schema/goods_cate")
+} = require("../schema/goods_cate");
 
-const { addCate, getAll, deleteCate, batchDeleteCate } = require("../controller/goods_cate")
+const {
+	getAll,
+	getSingle,
+	addCate,
+	updateCate,
+	deleteCate,
+	batchDeleteCate
+} = require("../controller/goods_cate");
 
 const goodsCateRouter = express.Router();
 
-/* 获取商品分类 */
+/* 获取所有商品分类 */
 goodsCateRouter.get("/getall", getAll);
 
-/* 添加分类 */
+/* 获取单个商品分类 */
+goodsCateRouter.get("/getsingle", expressJoi(goods_cate_id), getSingle);
+
+/* 添加商品分类 */
 goodsCateRouter.post("/add", expressJoi(goods_cate_add), addCate);
+
+/* 修改商品分类 */
+goodsCateRouter.post("/update", expressJoi(goods_cate_update), updateCate);
 
 /* 删除单个商品分类 */
 goodsCateRouter.delete("/delete",expressJoi(goods_cate_id), deleteCate);
