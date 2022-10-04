f7f0c1524138e34f519e656de1fdd316d1a25e1c
huanglin <2762565371@qq.com>
2022-10-04 20:07:22 +0800

    ✨ feat:完成查询所有商品分类，删除商品分类接口

--- controller/goods_cate.js
+++ controller/goods_cate.js
@@ -1,16 +1,33 @@
 const { DB_QUERY } = require("../db/index");
 const { tables } = require("../config");
 
+// 查询未被删除商品分类的 SQL 语句
+const selectAll = `SELECT id, name, alias, status FROM ${tables.goodsCate} WHERE is_delete=0`;
 // 查询 分类名称和分类别名 是否被占用的 SQL 语句
 const selectSQL = `SELECT * FROM ${tables.goodsCate} WHERE name=? OR alias=?`;
 // 添加商品分类的 SQL 语句
 const insertSQL = `INSERT INTO ${tables.goodsCate} SET ?`;
+// 删除商品分类的 SQL 语句
+const deleteSQL = `UPDATE ${tables.goodsCate} SET is_delete=1 WHERE id=?`;
+// 批量删除商品分类的 SQL 语句
+const batchdeleteSQL = `UPDATE ${tables.goodsCate} SET is_delete=1 WHERE FIND_IN_SET (id, ?)`;
 
+/* 获取所有商品分类的处理函数 */
+exports.getAll = async (_, response) => {
+	try {
+		const selectResult = await DB_QUERY(selectAll);
+
+		response.fastSend("获取成功！", 2000, selectResult);
+	}
+	catch (error) {
+		response.fastSend(error)
+	}
+}
 
 /* 添加商品分类的处理函数 */
-exports.add = async (requset, response) => {
+exports.addCate = async (request, response) => {
 	try {
-		const { name, alias	} = requset.body;
+		const { name, alias	} = request.body;
 
 		// 先查找数据库是否已有存在的 name 或者 alias
 		const selectResult = await DB_QUERY(selectSQL, [name, alias]);
@@ -47,5 +64,43 @@ exports.add = async (requset, response) => {
 	}
 	catch (error) {
 		response.fastSend(error);
+	}
+}
+
+/* 删除单个商品分类的处理函数 */
+exports.deleteCate = async (request, response) => {
+	try {
+		const { id } = request.query;
+
+		const deleteResult = await DB_QUERY(deleteSQL, id);
+
+		if (deleteResult.affectedRows !== 1) {
+			throw Error("删除商品分类失败！");
+		}
+		response.fastSend("删除商品分类成功！", 2000);
+
+	}
+	catch (error) {
+		response.fastSend(error);
+	}
+}
+
+/* 批量删除商品分类的处理函数 */
+exports.batchDeleteCate = async (request, response) => {
+	try {
+		const { ids } = request.body;
+
+		const idsStr = ids.join()
+
+		const result = await DB_QUERY(batchdeleteSQL, idsStr);
+
+		if (result.affectedRows !== ids.length) {
+			throw Error("删除商品分类失败！");
+		}
+
+		response.fastSend("删除商品分类成功！", 2000)
+	}
+	catch (error) {
+		response.fastSend(error)
 	}
 }
--- router/goods_cate.js
+++ router/goods_cate.js
@@ -2,14 +2,25 @@ const express = require("express");
 const expressJoi = require("@escook/express-joi");
 
 const {
-	goods_cate_add
+	goods_cate_id,
+	goods_cate_add,
+	goods_cate_batch_delete
 } = require("../schema/goods_cate")
 
-const { add } = require("../controller/goods_cate")
+const { addCate, getAll, deleteCate, batchDeleteCate } = require("../controller/goods_cate")
 
 const goodsCateRouter = express.Router();
 
+/* 获取商品分类 */
+goodsCateRouter.get("/getall", getAll);
+
 /* 添加分类 */
-goodsCateRouter.post("/add", expressJoi(goods_cate_add), add)
+goodsCateRouter.post("/add", expressJoi(goods_cate_add), addCate);
+
+/* 删除单个商品分类 */
+goodsCateRouter.delete("/delete",expressJoi(goods_cate_id), deleteCate);
+
+/* 批量删除商品分类 */
+goodsCateRouter.delete("/batchdelete",expressJoi(goods_cate_batch_delete), batchDeleteCate);
 
 module.exports = goodsCateRouter;
